6db83065fded2bff70e2eef87f060b515e793863
JD Jiang <jdjiang@jds-macbook-air.lan>
2025-09-10 20:26:34 -0400

    self attention impl by pytorch

---
+++ models/self_attention/self_attention_pytorch.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, d_model=2, row_dim=0, col_dim=1):
+        super().__init__()
+
+        self.W_q = nn.Linear(in_features=d_model,
+                             out_features=d_model,
+                             bias=False)
+        
+        self.W_k = nn.Linear(in_features=d_model,
+                             out_features=d_model,
+                             bias=False)
+        
+        self.W_v = nn.Linear(in_features=d_model,
+                             out_features=d_model,
+                             bias=False)
+        
+        # index of row and col dim
+        self.row_dim = row_dim
+        self.col_dim = col_dim
+
+    def forward(self, token_encodings):
+        q = self.W_q(token_encodings)
+        k = self.W_k(token_encodings)
+        v = self.W_v(token_encodings)
+
+        sims = torch.matmul(q, k.transpose(dim0=self.row_dim, dim1=self.col_dim))
+        scaled_sims = sims / torch.tensor(k.size(self.col_dim)**0.5)
+        attention_percents = F.softmax(scaled_sims, dim=self.col_dim)
+        attention_scores = torch.matmul(attention_percents, v)
+
+        return attention_scores
+    
+
+if __name__ == "__main__":
+    encoding_matrix = torch.tensor(
+        [[1.16, 0.23],
+        [0.57, 1.36],
+        [4.41, -2.16]]
+    )
+
+    # to make sure the result is always the same
+    torch.manual_seed(42)
+
+    selfAttention = SelfAttention(d_model=2, row_dim=0, col_dim=1)
+
+    result = selfAttention(encoding_matrix)
+    print(result)
